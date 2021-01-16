b7574c24795ca41569952658a01139fce0954b1a
harumi-matias <harumi.matias.is@usb.edu.mx>
2021-01-16 11:32:35 -0600

    imagen

---
+++ js/apuntes.js
@@ -0,0 +1,84 @@
+// 1. VARIABLES 
+// 1.1 var
+var nombre; /*Variable creada*/
+console.log(nombre);
+
+nombre="Harumi";
+console.log(nombre); /*se c oloca de nuevo para poder actualizar la página*/
+
+var nombre;
+var apellido;
+var edad;
+
+apellido="Matias" 
+edad=30 /* a los numeros no se les pone comillas porque no es una cadena de texto*/
+
+console.log(nombre);
+console.log(apellido);
+console.log(edad);
+
+var nombre01, 
+    apellido01, 
+    edad01;
+
+    nombre01="Paola";
+    apellido01="Ishizu";
+    edad01="30"
+
+    console.log (nombre,apellido,edad);
+    console.log (nombre01,apellido01,edad01);
+
+    //
+    var nombreProfesor="José";
+    console.log (nombreProfesor)
+
+    //1.2 let Te permite cambiar el dato pero no puedes volver a declarar con LET
+    let nombreAlumno="Juan";
+
+        nombreAlumno="Marcos"; /* se puede reasignar otro dato a la variable */
+
+    console.log(nombreAlumno);
+
+    //1.3 const Una vez que la declaras tienes que asignarle un valor porque es una constante
+
+    const nombreAlumno01="Jesus";
+    console.log(nombreAlumno01);
+
+    //DATOS
+    //2.1 Números 
+    let precio=32;
+    console.log(precio);
+
+    //String
+    precio="32"
+    console.log(precio);
+
+    //2.3 Booleano Falso o Verdadero / comparaciones
+    precio=true;
+    console.log(precio);
+
+    //OPERADORES
+    //3.1 Suma 
+    /* INTRODUCCION A FUNCIONES */
+    console.clear(); //limpia la consola//
+
+    const numero1 = 20,
+          numero2 = 30,
+          numero3 = "10",
+          numero4 = 10.50,
+          numero5 = -10.50;
+
+    let resultado;
+    
+    //suma
+    resultado=numero1+numero2;
+    //resta
+    resultado=numero4-numero5;
+    //multiplicación
+    resultado=numero1*numero2;
+    // división
+    resultado=numero1/numero2
+
+
+    console.log(resultado);
+
